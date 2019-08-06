f54fa21653bae7b536d64b43635393de9f084141
LauriMauranen <late.pamme@hotmail.com>
2019-08-06 16:30:56 +0300

    Lisätty STATIC_ROOT settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd syntiappi && gunicorn syntiappi.wsgi
--- syntiappi/syntiappi/settings.py
+++ syntiappi/syntiappi/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for syntiappi project.
 
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+django_heroku.settings(locals())
