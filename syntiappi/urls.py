b12539126016fd9c091a935602e07bbb69d6d42b
LauriMauranen <late.pamme@hotmail.com>
2019-08-06 18:45:16 +0300

    joo-o

--- syntiappi/urls.py
+++ syntiappi/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('synnit/', include('synnit.urls')),
+    path('', include('synnit.urls')),
     path('admin/', admin.site.urls),
 ]
